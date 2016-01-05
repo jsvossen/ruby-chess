b58290f78473adb97cbbc390297386feb48c2cf5
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-01-05 10:50:52 -0500

    add test for initial game state

--- spec/chessgame_spec.rb
+++ spec/chessgame_spec.rb
@@ -1,1 +1,48 @@
-requre_relative "spec_helper"
+require_relative "spec_helper"
+
+describe ChessGame do
+	
+	let(:game) { ChessGame.new }
+	before(:each) { allow(game).to receive(:puts) }
+
+	describe "#new" do
+		
+		it "creates a chess board with pieces" do
+			wpawn = "\u2659".encode('utf-8')
+			bpawn = "\u265F".encode('utf-8')
+			wrook = "\u2656".encode('utf-8')
+			brook = "\u265C".encode('utf-8')
+			wknight = "\u2658".encode('utf-8')
+			bknight = "\u265E".encode('utf-8')
+			wbish = "\u2657".encode('utf-8')
+			bbish = "\u265D".encode('utf-8')
+			wqueen = "\u2655".encode('utf-8')
+			bqueen = "\u265B".encode('utf-8')
+			wking = "\u2654".encode('utf-8')
+			bking = "\u265A".encode('utf-8')
+
+			expect(game.board).to receive(:puts).with("    a   b   c   d   e   f   g   h").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("8 | #{brook} | #{bknight} | #{bbish} | #{bqueen} | #{bking} | #{bbish} | #{bknight} | #{brook} | 8").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("7 | #{bpawn} | #{bpawn} | #{bpawn} | #{bpawn} | #{bpawn} | #{bpawn} | #{bpawn} | #{bpawn} | 7").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("6 |   |   |   |   |   |   |   |   | 6").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("5 |   |   |   |   |   |   |   |   | 5").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("4 |   |   |   |   |   |   |   |   | 4").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("3 |   |   |   |   |   |   |   |   | 3").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("2 | #{wpawn} | #{wpawn} | #{wpawn} | #{wpawn} | #{wpawn} | #{wpawn} | #{wpawn} | #{wpawn} | 2").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("1 | #{wrook} | #{wknight} | #{wbish} | #{wqueen} | #{wking} | #{wbish} | #{wknight} | #{wrook} | 1").ordered
+			expect(game.board).to receive(:puts).with("  |-------------------------------|").ordered
+			expect(game.board).to receive(:puts).with("    a   b   c   d   e   f   g   h").ordered
+			game.board.draw
+		end
+
+	end
+
+end
