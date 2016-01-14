b411b6646f9a79274b5ccb5dbe58372d7843858f
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-01-13 19:46:34 -0500

    refactor pawn tests

--- spec/chessgame_spec.rb
+++ spec/chessgame_spec.rb
@@ -124,35 +124,49 @@ describe ChessGame do
 	describe "#get_moves" do
 		
 		context "pawn" do
-			before(:each) { @p1 = game.board.square(1,2).occupant }
-
+			before(:each) do
+				game.board.empty
+				@p1 = Pawn.new("P",:white)
+			end
 			it "can move forward one or two spaces from home row" do
+				@p1.coord = game.board.square(1,2)
+				game.get_moves(@p1)
 				expect(@p1.moves).to eq [[1,4],[1,3]]
 			end
 			it "can only move forward one space outside of home row" do
-				game.move("P",1,4)
+				@p1.coord = game.board.square(1,3)
 				game.get_moves(@p1)
-				expect(@p1.moves).to eq [[1,5]]
+				expect(@p1.moves).to eq [[1,4]]
 			end
 			it "can capture opponent piece on forward diagonals" do
-				game.move("P",1,4)
-				game.active = game.players[1]
-				game.move("P",2,5)
+				@p1.coord = game.board.square(1,4)
+				p2 = Pawn.new("P",:black)
+				p2.coord = game.board.square(2,5)
 				game.get_moves(@p1)
 				expect(@p1.moves.include? [2,5]).to eq true					
 			end
+			it "cannot capture ally piece" do
+				@p1.coord = game.board.square(1,4)
+				p2 = Pawn.new("P",:white)
+				p2.coord = game.board.square(2,5)
+				game.get_moves(@p1)
+				expect(@p1.moves.include? [2,5]).to eq false					
+			end
 			it "cannot jump pieces" do
-				game.move("N",1,3)
+				@p1.coord = game.board.square(1,2)
+				n1 = Knight.new("N",:white)
+				n1.coord = game.board.square(1,3)
 				game.get_moves(@p1)
 				expect(@p1.moves.include? [1,4]).to eq false
 			end
 			it "cannot move forward on top of another piece" do
-				game.move("N",1,3)
+				@p1.coord = game.board.square(1,2)
+				n1 = Knight.new("N",:white)
+				n1.coord = game.board.square(1,3)
 				game.get_moves(@p1)
 				expect(@p1.moves.include? [1,3]).to eq false
 			end
 			it "cannot move off of board" do
-				game.board.square(1,2).occupant, game.board.square(1,8).occupant = nil
 				@p1.coord = game.board.square(1,8)
 				game.get_moves(@p1)
 				expect(@p1.moves).to eq []
@@ -164,7 +178,6 @@ describe ChessGame do
 				game.board.empty
 				@n1 = Knight.new("N",:white)
 			end
-
 			it "can move in an L pattern" do
 				@n1.coord = game.board.square(4,4)
 				game.get_moves(@n1)
@@ -177,7 +190,7 @@ describe ChessGame do
 				game.get_moves(@n1)
 				expect(@n1.moves.include? [3,6]).to eq true
 			end
-			it "cannot move on top of ally piece" do
+			it "cannot capture ally piece" do
 				@n1.coord = game.board.square(4,4)
 				p = Pawn.new("P",:white)
 				p.coord = game.board.square(3,6)
