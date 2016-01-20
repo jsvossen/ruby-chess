f488078a8fdc8eb6f92872bad30d9348faab48c5
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-01-20 12:09:40 -0500

    refactor help

--- lib/Help.rb
+++ lib/Help.rb
@@ -2,50 +2,38 @@ module Help
 
 	def help
 		puts "\n*** Help Menu ***"
-		puts "[1] How to Play"
-		puts "[2] Misc Commands"
+		puts "[1] Starting a Game"
+		puts "[2] Basic Movement"
+		puts "[3] Special Moves/Commands"
+		puts "[4] Saving a Game"
+		puts "[5] Misc Commands"
+		puts "*** End ***"
 		puts "[close] return to game"
 		input = Readline.readline("help: ").downcase
-		until input == "1" || input == "2" || input == "close" do
-			puts "Invalid input. Enter [1], [2], or [close] to return to game."
+		until input.to_i.between?(1,5) || input == "close" do
+			puts "Invalid input. Enter a number or [close] to return to game."
 			input = Readline.readline("help: ").downcase
 		end
 		case input.downcase
 			when "1"
-				play_menu
+				start_game_commands
 			when "2"
-				misc_menu
+				movement_commands
+			when "3"
+				special_commands
+			when "4"
+				saving_commands
+			when "5"
+				misc_commands
 		end
 	end
 
-	def play_menu
-		puts "\n*** How to Play ***"
-		puts "[1] Moving Pieces"
-		puts "[2] Special Commands"
-		puts "*** End ***"
-		puts "[back] previous menu"
-		puts "[close] return to game"
-		input = Readline.readline("help: ").downcase
-		until input == "1" || input == "2" || input == "back" do
-			puts "Invalid input. Enter [1], [2], [close], or [back]."
-			input = Readline.readline("help: ").downcase
-		end
-		case input
-			when "1"
-				movement
-			when "2"
-				special
-			when "back"
-				help
-		end
-	end
-
-	def movement
+	def movement_commands
 		puts "\n*** Movement Commands ***"
 		puts "Enter moves in standard chess notation; ie piece-rank-file. E.g:"
 		puts "[Qg5] = queen moves to the g-file and 5th rank (square g5)"
 		puts "If more than one piece can move to the destination square, disambiguate \nby including the origin's rank, file, or both. E.g:"
-		puts "[Ngf3] = knight from the g-file moves to the square f3"
+		puts "[Ngf3] or [N1f3] or [Ng1f3] = knight from the g-file/1-rank moves to square f3"
 		puts "*** Piece Abbreviations ***"
 		puts "[P] pawn"
 		puts "[R] rook"
@@ -54,88 +42,80 @@ module Help
 		puts "[Q] queen"
 		puts "[K] king"
 		puts "*** End ***"
-		puts "[back] previous menu"
+		puts "[back] back to menu"
 		puts "[close] return to game"
 		input = Readline.readline("help: ").downcase
 		until input == "back" || input == "close" do
 			puts "Invalid input. Enter [close] or [back]."
 			input = Readline.readline("help: ").downcase
 		end
-		play_menu if (input == "back")
+		help if (input == "back")
 	end
 
-	def special
+	def special_commands
 		puts "\n*** Special Commands ***"
 		puts "[draw] request a draw; the player who proposes the draw makes one more move, \nthen the opponent may either agree to the draw or reject it to continue play."
 		puts "[resign] concede the game"
 		puts "Castling is not available at this time."
 		puts "*** End ***"
-		puts "[back] previous menu"
+		puts "[back] back to menu"
 		puts "[close] return to game"
 		input = Readline.readline("help: ").downcase
 		until input == "back" || input == "close" do
 			puts "Invalid input. Enter [close] or [back]."
 			input = Readline.readline("help: ").downcase
 		end
-		play_menu if (input == "back")
+		help if (input == "back")
 	end
 
-	def misc_menu
-		puts "\n*** Misc Commands ***"
-		puts "[1] out-of-game commands"
-		puts "[2] in-game commands"
-		puts "*** End ***"
-		puts "[back] previous menu"
-		puts "[close] return to game"
-		input = Readline.readline("help: ").downcase
-		until input == "1" || input == "2" || input == "back" || input == "close" do
-			puts "Invalid input. Enter [1], [2], [close], or [back]."
-			input = Readline.readline("help: ").downcase
-		end
-		case input.downcase
-			when "1"
-				misc_oo_game
-			when "2"
-				misc_in_game
-			when "back"
-				help
-		end
-	end
-
-	def misc_oo_game
-		puts "\n*** Out-of-Game Commands ***"
+	def start_game_commands
+		puts "\n*** Starting a Game ***"
 		puts "[new] start a new game"
 		puts "[load] show save list to select a game to load"
 		puts "[load <name>] load game file <name>"
-		puts "[help] show help menu"
-		puts "[quit] or [exit] exit program"
+		puts "Games can only be started/loaded from the welcome screen."
 		puts "*** End ***"
-		puts "[back] previous menu"
+		puts "[back] back to menu"
 		puts "[close] return to game"
 		input = Readline.readline("help: ").downcase
 		until input == "back" || input == "close" do
 			puts "Invalid input. Enter [close] or [back]."
 			input = Readline.readline("help: ").downcase
 		end
-		misc_menu if (input == "back")
+		help if (input == "back")
 	end
 
-	def misc_in_game
-		puts "\n*** In-Game Commands ***"
+	def saving_commands
+		puts "\n*** Saving a Game ***"
 		puts "[save] save over game that was most recently saved or loaded"
 		puts "[save <name>] create new save or overwrites <name> if it exists; save names may not include spaces"
 		puts "[show saves] list existing save files"
-		puts "[help] show help menu"
-		puts "[exit] exit game and return to main menu; abandon unsaved progress"
-		puts "[quit] quit program; abandon unsaved progress"
+		puts "Saving is only available in-game."
 		puts "*** End ***"
-		puts "[back] previous menu"
+		puts "[back] back to menu"
 		puts "[close] return to game"
 		input = Readline.readline("help: ").downcase
 		until input == "back" || input == "close" do
 			puts "Invalid input. Enter [close] or [back]."
 			input = Readline.readline("help: ").downcase
 		end
-		misc_menu if (input == "back")
+		help if (input == "back")
 	end
+
+	def misc_commands
+		puts "\n*** Misc Commands ***"
+		puts "[help] show help menu"
+		puts "[exit] exit game and return to welcome screen; abandon unsaved progress"
+		puts "[quit] quit program; abandon unsaved progress"
+		puts "*** End ***"
+		puts "[back] back to menu"
+		puts "[close] return to game"
+		input = Readline.readline("help: ").downcase
+		until input == "back" || input == "close" do
+			puts "Invalid input. Enter [close] or [back]."
+			input = Readline.readline("help: ").downcase
+		end
+		help if (input == "back")
+	end
+
 end
